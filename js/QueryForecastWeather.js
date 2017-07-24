fa3b2d5e2ad957e247efd959ba242a63cf21e7b9
Ekaterina <ekaterina.gargesyan@gmail.com>
2017-07-24 17:34:32 +0300

    add query forecast weather to the server ApixuAPI

---
+++ js/QueryForecastWeather.js
@@ -0,0 +1,12 @@
+function query(place) {
+    var query = new XMLHttpRequest();
+
+    query.addEventListener("load", function (event) {
+        dataWeather = JSON.parse(event.currentTarget.responseText);
+    });
+
+    query.open("GET", `https://api.apixu.com/v1/forecast.json?key=c13f00aa4d97425298872829170307&q=${place}&days=5`);
+    query.send();
+
+    return query;
+}
