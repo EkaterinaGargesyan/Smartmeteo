303e9e834be6b51f3482887086a5246b9afcd09b
Ekaterina <ekaterina.gargesyan@gmail.com>
2017-07-24 17:38:01 +0300

    add autocomlete regions & geocoding coords

---
+++ js/AutocompleteRegions&GeocodingCoords.js
@@ -0,0 +1,18 @@
+// make form witn autocomplete for return place
+var autocomplete = new google.maps.places.Autocomplete(
+    (document.querySelector(".start-page__select-region")),
+    {types: ["(cities)"],
+        /*componentRestrictions: {country: "us"}*/
+    });
+
+// find coordinates place which user type-in search-form
+function find (address, cb) {
+    var geocoder = new google.maps.Geocoder;
+    geocoder.geocode({"address": address}, function(results) {
+        var lat = results[0].geometry.location.lat();
+        var lng = results[0].geometry.location.lng();
+        var coords =  `${lat},${lng}`;
+
+        cb(coords);
+    });
+}
